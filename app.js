a3292010e0c1ae65f6456030866ae6971b55e35e
TommyHerout <tommyherout@gmail.com>
2021-03-04 18:04:00 +0100

    add horizontal scrolling

--- app.js
+++ app.js
@@ -31,3 +31,17 @@ to(".girl-fading", {
 "sameTime")
 
 
+let container = document.getElementById("container");
+
+gsap.to(container, {
+  x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    invalidateOnRefresh: true,
+    pin: true,
+    scrub: 1,
+    end: () => "+=" + container.offsetWidth
+  }
+})
+
