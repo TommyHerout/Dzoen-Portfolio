ff4c601dc57cb7620166e41270466cad7e94374f
TommyHerout <tommyherout@gmail.com>
2021-03-05 17:02:37 +0100

    add burger responsive menu in another page

---
+++ art.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        // TOGGLE NAVBAR
+        nav.classList.toggle('nav-active');
+
+        // ANIMATION
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 2.5}s`;
+            }
+        });
+
+        // BURGER
+        burger.classList.toggle('toggle')
+    });
+}
+
+navSlide();
