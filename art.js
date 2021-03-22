d299be7f825626d48de5214cec76f670aac27e96
TommyHerout <tommyherout@gmail.com>
2021-03-22 12:02:18 +0100

    add art design styling with welcome text

--- art.js
+++ art.js
@@ -52,3 +52,14 @@ const navSlide = () => {
 }
 
 navSlide();
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
