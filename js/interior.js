791463a23cd92602929122537d786304976012aa
TommyHerout <tommyherout@gmail.com>
2021-03-25 20:27:35 +0100

    fix fkn bug on mobile devices with scrolling padding over the toolbox

--- js/interior.js
+++ js/interior.js
@@ -51,3 +51,20 @@ navSlide();
 
 console.log("WINDOW HEIGHT " + window.innerHeight);
 console.log("WINDOW WIDTH " + window.innerWidth);
+
+var height = window.innerHeight;
+var a = setInterval(function() {
+    $(window).scrollTop(-1);
+    resize();
+}, 500); // Don't lower more than 500ms, otherwise there will be animation-problems with the  Safari toolbar
+
+$(window).on('resize', function() {
+    resize();
+});
+
+var resize = function() {
+    if(window.innerHeight != height) {
+        height = window.innerHeight;
+        $('.section').css('height', height + 'px');
+    }
+};
