03ede9018033267020ed28e50e7c52683ad528d5
TommyHerout <tommyherout@gmail.com>
2021-03-25 14:02:29 +0100

    add footer to interior page

--- js/interior.js
+++ js/interior.js
@@ -12,7 +12,7 @@ new fullpage("#fullpage", {
     const description2 = section.querySelectorAll(".description-n2");
     const tl = new TimelineMax({ delay: 0.5 });
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
-    if(destination.index === 2){
+    if(destination.index === 2 || destination.index === 4){
         tl.fromTo(description, 1, { x: "-80%", opacity: 0 }, { x: "-40%", opacity: 1 });
     }else{
         tl.fromTo(description, 1, { x: "40%", opacity: 0 }, { x: "-40%", opacity: 1 });
